0beacd0a3bf780bb2f5be383ea2c6edd9b0204d3
JeffEngeling <143764811+JeffEngeling@users.noreply.github.com>
2023-09-01 19:05:08 -0500

    Add files via upload

--- SQLPortfolioProject1CovidAnalysis.sql
+++ SQLPortfolioProject1CovidAnalysis.sql
@@ -1,4 +1,11 @@
 
+/*
+Covid 19 Data Exploration
+
+Skills used: Joins, Aggregate Functions, Converting Data Types, CTE's, Temp Tables, Windows Functions
+
+*/
+
 SELECT *
 FROM PortfolioProject..CovidDeaths
 WHERE continent is NOT NULL
@@ -8,14 +15,16 @@ ORDER BY 3,4
 --FROM PortfolioProject..CovidVaccinations
 --ORDER BY 3,4
 
--- Selecting Data that I will be using
+-- Selecting Data that we will be starting with
 
 Select Location, date, total_cases, new_cases, total_deaths, population
 FROM PortfolioProject..CovidDeaths
+WHERE continent IS NOT NULL
 ORDER BY 1,2
 
--- Looking at Total Cases vs Total Deaths
--- Shows the percentage of cases resulting in death by different countries
+-- Total Cases vs Total Deaths
+-- Shows the percentage of cases resulting in death in different countries
+
 Select Location, date, total_cases, total_deaths, 
 (CONVERT(float,total_deaths)/NULLIF(CONVERT(float,total_cases),0))*100 AS DeathPercentage
 FROM PortfolioProject..CovidDeaths
@@ -29,7 +38,7 @@ FROM PortfolioProject..CovidDeaths
 WHERE Location like '%states'
 ORDER BY 1,2
 
--- Looking at Total Cases vs Population
+-- Total Cases vs Population
 -- Shows what percentage of population contracted Covid
 
 Select Location, date, Population, total_cases,  
@@ -38,7 +47,7 @@ FROM PortfolioProject..CovidDeaths
 WHERE Location like '%states'
 ORDER BY 1,2
 
--- Looking at countries with highest infection rate compared to population
+-- Countries with highest infection rate compared to population
 -- there was an issue with the MAX() fx and data type for total_cases -> cast as int and it resolved the problem
 
 Select Location, Population, MAX(cast(total_cases as int)) AS HighestInfectionCount,
@@ -49,13 +58,13 @@ ORDER BY PercentPopulationInfected DESC
 
 -- Showing Countries with Highest Death Count per Population
 
-Select Location, continent, MAX(cast(total_deaths as int)) AS TotalDeathCount
+Select Location,MAX(cast(total_deaths as int)) AS TotalDeathCount
 FROM PortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
-GROUP BY Location, continent
+GROUP BY Location
 ORDER BY TotalDeathCount DESC
 
--- Pulling data broken out by continent
+-- Sorting Death Count by Continent
 
 Select continent, SUM(cast(new_deaths as int)) AS TotalDeathCount
 FROM PortfolioProject..CovidDeaths
@@ -71,7 +80,7 @@ WHERE continent IS NOT NULL
 GROUP BY date
 ORDER BY 1,2
 
--- Overall Global Totals start of pandemic to August 2023
+-- Overall Global Totals from start of pandemic to August 2023
 Select SUM(new_cases) AS TotalCases, SUM(cast(new_deaths as int)) AS TotalDeaths, SUM(cast(new_deaths as int))/NULLIF(SUM(new_cases),0) *100 AS WorldDeathPercentage
 FROM PortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
@@ -80,7 +89,8 @@ ORDER BY 1,2
 SELECT *
 FROM PortfolioProject..CovidVaccinations
 
--- Looking at Total Population vs Vaccinations
+-- Total Population vs Vaccinations
+-- Shows Percent of Population that has received at least one Covid vaccine
 
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 FROM PortfolioProject..CovidDeaths dea
@@ -101,7 +111,7 @@ JOIN PortfolioProject..CovidVaccinations vac
 WHERE dea.continent IS NOT NULL
 ORDER BY 2,3
 
--- Using a CTE to reflect % population vaccinated
+-- Using a CTE to perform Calculation on Partition By on the rolling count of population vaccinated
 
 WITH PopVsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 AS
@@ -117,7 +127,7 @@ WHERE dea.continent IS NOT NULL
 SELECT *, (RollingPeopleVaccinated/Population) *100
 FROM PopVsVac
 
--- Using a Temp Table to reflect % population vaccinated
+-- Using a Temp Table to perfrom Calculation on Partition By on the rolling count of population vaccinated
 
 DROP TABLE IF EXISTS #PercentPopVaccinated
 CREATE TABLE #PercentPopVaccinated
